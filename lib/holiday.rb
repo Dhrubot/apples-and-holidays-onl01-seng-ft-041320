eabb32f77d6983aa98e238dab570e6b9bac9d011
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-19 05:20:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,9 @@ def all_supplies_in_holidays(holiday_hash)
       season_str = season.to_s.capitalize!
       holiday_name_str = holiday_name.to_s.split("_").map {|word| word.capitalize}.join(" ")
       supply = supplies.join(", ")
-      puts 
+      puts <<MINE
+      
+      MINE
     end
   end
 end
