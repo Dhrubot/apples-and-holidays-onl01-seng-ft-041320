c1716de20a29277359a3b27837b3d17bd67cbcdc
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-19 04:38:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,8 +65,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |season, holiday|
-    
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday_name, supplies|
+      puts ""
+    end
   end
 end
 
